15a8a4d2cc291fdda2738e9eae032ee25df7bb91
Abdulhazeem Morakinyo <130643850+Abdulhazeem01@users.noreply.github.com>
2025-08-19 10:21:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;PBkWhD4b#zm8LBBfmq3WNfHJ1FAtgnEUnojkox5O8Ii9VacSYMxs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2349067233220", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
